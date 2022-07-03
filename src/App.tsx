bcccf5c9b280123208dfffa2c690b73bae5429e5
Dhrumil Shah <contact@iamdhrumilshah.com>
2022-07-03 02:04:09 -0400

    Updated main homepage with new user actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React, {Fragment, ReactNode} from 'react';
+import React, {ReactNode} from 'react';
 import './App.css';
 import {Instructions} from "./Instructions/Instructions";
 import Navigation from "./Navigation";
-import {Route, Router, Routes} from "react-router";
+import {Route, Routes} from "react-router";
 import {BrowserRouter} from "react-router-dom";
 import {Home} from "./Home/Home";
+import {Legal} from "./Legal/Legal";
 
 class App extends React.Component {
-  public render(): ReactNode {
-    return (
-      <div>
+    public render(): ReactNode {
+        return (
             <BrowserRouter>
-                <Routes>
-                    <Route path="/:userId" element={<Home/>}/>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/instructions" element={<Instructions/>}/>
-                </Routes>
+                <div className="content">
+                    <Routes>
+                        <Route path="/:userId" element={<Home/>}/>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/instructions" element={<Instructions/>}/>
+                        <Route path="/legal" element={<Legal/>}/>
+                    </Routes>
+                </div>
                 <Navigation></Navigation>
             </BrowserRouter>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
