10230cf9b3feba8d177ca7f0c33c08df830f2571
Dhrumil Shah <contact@iamdhrumilshah.com>
2022-07-03 02:02:39 -0400

    Create variables.tsx

---
+++ src/other/variables.tsx
@@ -0,0 +1,7 @@
+export const WEBSITE_NAME: string = "SpreadYourLinks";
+export const CONTACT_EMAIL: string = "spreadyourlinks@iamdhrumilshah.com";
+export const ADDITIONAL_SERVICES: string = "Google Analytics, Google Cloud Platform, Heroku, Google Web Safe API, CloudFlare, MongoDB Cloud, and Github";
+
+export function WEBSITE_URL(): string {
+    return window.location.hostname;
+}
