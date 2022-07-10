97ca834c8c0d04ec1189e885cfa6a35ef212977f
Dhrumil Shah <contact@iamdhrumilshah.com>
2022-07-10 17:56:30 -0400

    Added a loading skeleton and error messages

--- src/LoggedInHome/LoggedInHome.tsx
+++ src/LoggedInHome/LoggedInHome.tsx
@@ -1,12 +1,14 @@
 import React, {ReactElement, useEffect, useState} from "react";
 import './LoggedInHome.css';
-import {Button, Chip, TextField, Tooltip, Zoom} from "@mui/material";
+import {Button, Chip, Skeleton, TextField, Tooltip, Zoom} from "@mui/material";
 import {ArrowForwardIos, Logout, OpenInFull} from "@mui/icons-material";
 import {redirect, removeSecretCodeCookie, setSecretCodeCookie} from "../other/cookies";
 import {WEBSITE_NAME} from "../other/variables";
 import {Link} from "react-router-dom";
-import restCalls from "../other/restCalls";
+import restCalls, {getLinkResponse} from "../other/restCalls";
 import BookmarkIcon from '@mui/icons-material/Bookmark';
+import {toast, ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 interface LoggedInHomeProps {
     id: string
@@ -17,6 +19,7 @@ interface LoggedInHomeState {
 }
 
 export default function LoggedInHome(props: LoggedInHomeProps): ReactElement {
+    const missingSecretCodeRedirectTime = 5000;
     const [state, setState] = useState<LoggedInHomeState>({link: "Loading..."});
     setSecretCodeCookie(props.id);
 
@@ -27,8 +30,21 @@ export default function LoggedInHome(props: LoggedInHomeProps): ReactElement {
                 return;
             }
         }
-        restCalls.getLink(props.id).then((link: string) => {
-            setState({link: link});
+        restCalls.getLink(props.id).then((response: getLinkResponse) => {
+            if (response.error) {
+                toast.error(response.error, {
+                    position: "bottom-left",
+                    autoClose: missingSecretCodeRedirectTime,
+                    hideProgressBar: false,
+                    closeOnClick: false,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+                setTimeout(() => logout(), missingSecretCodeRedirectTime + 1500);
+                return;
+            }
+            setState({link: response.link});
         });
     }, [props.id]);
 
@@ -43,36 +59,72 @@ export default function LoggedInHome(props: LoggedInHomeProps): ReactElement {
         }
     }
 
+    function logout() {
+        removeSecretCodeCookie();
+        redirect('/');
+    }
 
+    const loadingElement: ReactElement = (
+        <>
+            <Skeleton
+                variant="rectangular"
+                width='50vw'
+                height={60}
+            />
+            <div className="loading-redirect-buttons-container">
+                <Skeleton
+                    className="loading-redirect-buttons"
+                    variant="rectangular"
+                    style={{left: 0}}
+                />
+                <Skeleton
+                    variant="rectangular"
+                    className="loading-redirect-buttons"
+                    style={{right: 0}}
+                />
+            </div>
+        </>
+    )
+
+
+    // @ts-ignore
     return (
         <div className="App-header App">
             <h1>{WEBSITE_NAME}</h1>
             <p style={{fontSize: '0.4em'}}>By using this service you agree to the Terms and Conditions, and Privacy
                 Policy at <Link className="link-changed" to="/legal">/legal</Link>.</p>
-            <div>
-                <Tooltip disableHoverListener={isLoading()} arrow TransitionComponent={Zoom}
-                         title="Copy link to clipboard">
-                    <Button onClick={handleFocus}>
-                        <TextField id="redirect-link" onFocus={handleFocus} disabled
-                                   value={state.link}/>
-                    </Button>
-                </Tooltip>
-            </div>
-            <div className="redirect-buttons">
-                <Button disabled={isLoading()}
-                        onClick={() => redirect(`https://www.youtube.com/redirect?q=${state.link}`)}
-                        size="large" variant="outlined">
-                    Full screen
-                    <OpenInFull className="redirect-button-icon"/>
-                    <Chip className="redirect-button-icon" disabled label="Tesla Only"/>
-                </Button>
 
-                <Button disabled={isLoading()} onClick={() => redirect(state.link)} size="large"
-                        variant="contained">
-                    Current window
-                    <ArrowForwardIos className="redirect-button-icon"/>
-                </Button>
-            </div>
+
+            {isLoading() ? loadingElement :
+
+                <>
+                    <div>
+                        <Tooltip disableHoverListener={isLoading()} arrow TransitionComponent={Zoom}
+                                 title="Copy link to clipboard">
+                            <Button onClick={handleFocus}>
+                                <TextField id="redirect-link" onFocus={handleFocus} disabled
+                                           value={state.link}/>
+                            </Button>
+                        </Tooltip>
+                    </div>
+
+
+                    <div className="redirect-buttons">
+                        <Button disabled={isLoading()}
+                                onClick={() => redirect(`https://www.youtube.com/redirect?q=${state.link}`)}
+                                size="large" variant="outlined">
+                            Full screen
+                            <OpenInFull className="redirect-button-icon"/>
+                            <Chip className="redirect-button-icon" disabled label="Tesla Only"/>
+                        </Button>
+
+                        <Button disabled={isLoading()} onClick={() => redirect(state.link)} size="large"
+                                variant="contained">
+                            Current window
+                            <ArrowForwardIos className="redirect-button-icon"/>
+                        </Button>
+                    </div>
+                </>}
 
             <div className='top-buttons'>
                 <Chip className='bookmark-button-chip' color="warning" label={
@@ -82,12 +134,11 @@ export default function LoggedInHome(props: LoggedInHomeProps): ReactElement {
                 }/>
 
                 <Tooltip arrow TransitionComponent={Zoom} title="Logout">
-                    <Chip className='logout-button' label={<Logout fontSize='small'/>} onClick={() => {
-                        removeSecretCodeCookie();
-                        redirect('/');
-                    }} color="error"/>
+                    <Chip className='logout-button' label={<Logout fontSize='small'/>} onClick={() => logout()}
+                          color="error"/>
                 </Tooltip>
             </div>
+            <ToastContainer/>
         </div>
     );
 }
