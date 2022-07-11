94acf0147cc818f1625e3a8db0babbe5f9051562
Dhrumil Shah <contact@iamdhrumilshah.com>
2022-07-11 01:59:51 -0400

    Booklet workaround for security blocks

--- src/other/booklet.ts
+++ src/other/booklet.ts
@@ -1,20 +1,7 @@
 import {BACKEND_API} from "./variables";
 
-export function booklet(secretCode: string): string {
-    const actualCode = `
-        const xhr = new XMLHttpRequest();
-        xhr.addEventListener("readystatechange", function() {
-            if(this.readyState === 4) {
-                alert(JSON.parse(this.responseText).updated ? "Updated" : "Not Updated");
-            }
-        });
-        xhr.open("POST", "${BACKEND_API}/setLink");
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(JSON.stringify({
-            "secretCode": "${secretCode}",
-            "link": window.location.href
-        }));`
-        .replaceAll('\n', '')
+function bookletCleanup(booklet: string) {
+        return booklet.replaceAll('\n', '')
         .replaceAll(new RegExp(/\s*\{\s*/gm), '{')
         .replaceAll(new RegExp(/\s*}\s*/gm), '}')
         .replaceAll(new RegExp(/\s*\)\s*/gm), ')')
@@ -24,5 +11,24 @@ export function booklet(secretCode: string): string {
         .replaceAll(new RegExp(/\s*:\s*/gm), ':')
         .replaceAll(new RegExp(/\s*\?\s*/gm), '?')
         .replaceAll(new RegExp(/\s*;\s*/gm), ';');
+}
+
+export function booklet(secretCode: string): string {
+    const actualCode = bookletCleanup(`
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener("readystatechange", function() {
+            if(this.readyState === 4) {
+                alert(JSON.parse(this.responseText).updated ? "Updated" : "Not Updated");
+            }
+        });
+        xhr.onerror = function(e) {
+            window.open("${BACKEND_API}/setLink/${secretCode}/" + encodeURIComponent(window.location.href));
+        };
+        xhr.open("POST", "${BACKEND_API}/setLink");
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify({
+            "secretCode": "${secretCode}",
+            "link": window.location.href
+        }));`);
     return `javascript:${encodeURIComponent(`(function(){${actualCode.trim()}})();`)}`;
 }
