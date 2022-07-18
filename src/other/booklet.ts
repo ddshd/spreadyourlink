edd36c4fd4ae208b0deeb99832545030975a9f9c
Dhrumil Shah <contact@iamdhrumilshah.com>
2022-07-17 20:12:43 -0400

    Updated booklet to fix errors with iOS users

--- src/other/booklet.ts
+++ src/other/booklet.ts
@@ -7,7 +7,7 @@ function bookletCleanup(booklet: string) {
         .replaceAll(new RegExp(/\s*\)\s*/gm), ')')
         .replaceAll(new RegExp(/\s*\(\s*/gm), '(')
         .replaceAll(new RegExp(/\s*=\s*/gm), '=')
-        .replaceAll(new RegExp(/\s*,\s*/gm), ',')
+        .replaceAll(new RegExp(/\s+,\s+/gm), ',')
         .replaceAll(new RegExp(/\s*:\s*/gm), ':')
         .replaceAll(new RegExp(/\s*\?\s*/gm), '?')
         .replaceAll(new RegExp(/\s*\+\s*/gm), '+')
@@ -17,13 +17,15 @@ function bookletCleanup(booklet: string) {
 export function booklet(secretCode: string): string {
     /* eslint-disable */
     const actualCode = `
-        const windowReference = window.open();
         const xhr = new XMLHttpRequest();
         function openNewWindow() {
            const url = "${window.location.protocol}//${window.location.host}/setLink.html?secretCode=${secretCode}&link="
                     + encodeURIComponent(window.location.href);
            try {
-               windowReference.location = url;
+               var openedWindow = window.open(url);
+               if(!openedWindow || openedWindow.closed || typeof openedWindow.closed === "undefined")  {
+                throw new Error('Popup blocked, throwing exception to be caught.');
+               }
            }
            catch {
                 if (confirm(\"Your browser does not support any of our primary methods for updating the URL. We have one more method, however, this will cause your page to redirect. This may cause you to lose any information you have entered on this page. Is this okay?\")) {
@@ -34,10 +36,6 @@ export function booklet(secretCode: string): string {
         }
         xhr.addEventListener("readystatechange", function() {
             if(this.readyState === 4) {
-                if (this.responseText == "") {
-                    openNewWindow();
-                    return;
-                }
                 alert(JSON.parse(this.responseText).updated ? "Updated" : "Not Updated");
             }
         });
